0c9541db6665ff0ecfd7b7c5ba52e247bd783dac
Simon Hauser <Simon-Hauser@outlook.de>
2021-12-02 07:41:24 +0100

    a little bit of day01 cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -12,12 +12,8 @@ fn read_file(file: &str) -> Vec<i32> {
 
 fn ex1(input: &Vec<i32>) {
   let mut count = 0;
-  let mut prev = -1;
-  for n in input {
-    if prev == -1 {
-      prev = *n;
-      continue;
-    }
+  let mut prev = input[0];
+  for n in input.iter().skip(1) {
     if prev < *n {
       count += 1;
     }
@@ -28,13 +24,9 @@ fn ex1(input: &Vec<i32>) {
 
 fn ex2(input: &Vec<i32>) {
   let mut count = 0;
-  let mut prev = -1;
-  for i in 0..input.len() - 2 {
+  let mut prev = input[0] + input[1] + input[2];
+  for i in 1..input.len() - 2 {
     let sum = input[i] + input[i + 1] + input[i + 2];
-    if prev == -1 {
-      prev = sum;
-      continue;
-    }
     if prev < sum {
       count += 1;
     }
