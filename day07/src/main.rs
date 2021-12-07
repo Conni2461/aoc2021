9311471c9a66a365f10d364c236a7386cd7d0419
Simon Hauser <Simon-Hauser@outlook.de>
2021-12-07 07:18:19 +0100

    ferrisDance Day07

---
+++ day07/src/main.rs
@@ -0,0 +1,54 @@
+use std::cmp::{max, min};
+
+fn read_file(file: &str) -> Vec<i32> {
+  std::fs::read_to_string(file)
+    .unwrap()
+    .trim()
+    .split(',')
+    .map(|s| {
+      s.to_string()
+        .parse::<i32>()
+        .expect("All lines should be numbers")
+    })
+    .collect()
+}
+
+fn ex1(input: &[i32]) {
+  let max_destination = *input.iter().max().unwrap();
+  let mut min_fuel = i32::max_value();
+  for d in 1..max_destination {
+    let mut fuel = 0;
+    for crab in input {
+      fuel += max(crab, &d) - min(crab, &d);
+    }
+    if fuel < min_fuel {
+      min_fuel = fuel;
+    }
+  }
+  println!("{}", min_fuel);
+}
+
+fn real_crab_engineering(n: i32) -> i32 {
+  (n * (n + 1)) / 2
+}
+
+fn ex2(input: &[i32]) {
+  let max_destination = *input.iter().max().unwrap();
+  let mut min_fuel = i32::max_value();
+  for d in 1..max_destination {
+    let mut fuel = 0;
+    for crab in input {
+      fuel += real_crab_engineering(max(crab, &d) - min(crab, &d));
+    }
+    if fuel < min_fuel {
+      min_fuel = fuel;
+    }
+  }
+  println!("{}", min_fuel);
+}
+
+fn main() {
+  let input = read_file("data/input");
+  ex1(&input);
+  ex2(&input);
+}
